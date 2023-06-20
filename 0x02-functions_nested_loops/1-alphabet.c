6582bc2db1a45d82846a55396103c6c27814e05d
aviweM <aviwemvundlela@gmail.com>
2023-06-20 13:46:09 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print-alphabet - Print the alphabets,
+ * print-alphabet - Prints the alphabets,
  * in lowercase, followed by a new line.
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
@@ -16,5 +16,4 @@ void print_alphabet(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
 }
