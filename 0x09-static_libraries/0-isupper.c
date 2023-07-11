15eb7b0b9c600c12516d8335d49e4c95adc76c8d
aviweM <aviwemvundlela@gmail.com>
2023-07-10 18:42:40 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a letter is uppercase
+ * @c: char to check
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  _strlen - Returns the length of a string.
+ * @s: The string to get the length of
+ * Return: The length of @s.
+ */
+size_t _strlen(const char *s)
+
+{
+	size_t length = 0;
+
+	while (*s++)
+	length++;
+	return (length);
+
+}
