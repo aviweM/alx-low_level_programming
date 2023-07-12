b48deb55ed374fa6b791c2780d4b1f8ffcc9a1cc
aviweM <aviwemvundlela@gmail.com>
2023-07-11 17:05:56 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - Duplicate a string
+  * @str: the string to duplicate
+  *
+  * Return: the string duplicated
+  */
+char *_strdup(char *str)
+{
+	int a = 0, i = 1;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	p = malloc((sizeof(char) * i) + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		p[a] = str[a];
+		a++;
+	}
+
+	p[a] = '\0';
+	return (p);
+}
