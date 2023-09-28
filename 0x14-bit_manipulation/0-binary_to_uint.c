4f84d1b2827d66054ca75e13dc4c232f1f0dbae6
aviweM <aviwemvundlela@gmail.com>
2023-09-28 14:31:38 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,23 +2,23 @@
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int
- * @b: string containing the binary number 
+ * @b: string containing the binary number
  *
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int h;
+	int r;
 	unsigned int value = 0;
 
 	if (!b)
 		return (0);
 
-	for (h = 0; b[h]; h++)
+	for (r = 0; b[r]; r++)
 	{
-		if (b[h] < '0' || b[h] > '1')
+		if (b[r] < '0' || b[r] > '1')
 			return (0);
-		value = 2 * value + (b[h] - '0');
+		value = 2 * value + (b[r] - '0');
 	}
 
 	return (value);
