a7989d34b1c31d25bdde6de8f3123951d95af742
aviweM <aviwemvundlela@gmail.com>
2023-10-06 19:06:18 -0700

    -create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,9 @@ int create_file(const char *filename, char *text_content)
 	int r, s, index = 0;
 
 	if (filename == NULL)
+	{
 		return (-1);
-
+	}
 		if (text_content != NULL)
 		{
 			for (index = 0; text_content[index];)
